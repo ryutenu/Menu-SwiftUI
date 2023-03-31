ae06eeb29156dc3fd051afea498526375bbbbd1b
Alan Liu <>
2023-04-01 08:02:55 +0900

    [Mod] Added MenuData

---
+++ Menu-SwiftUI/Menu-SwiftUI/MenuData.swift
@@ -0,0 +1,14 @@
+//
+//  MenuData.swift
+//  Menu-SwiftUI
+//
+//  Created by Alan Liu on 2023/04/01.
+//
+
+import Foundation
+
+struct MenuData {
+    var name: String
+    var price: String
+    var imageName: String
+}
--- Menu-SwiftUI/Menu-SwiftUI/MenuView.swift
+++ Menu-SwiftUI/Menu-SwiftUI/MenuView.swift
@@ -8,6 +8,21 @@
 import SwiftUI
 
 struct MenuView: View {
+    
+    var menus: [MenuData] = [
+        MenuData(name: "Onigiri", price: "1.99", imageName: "onigiri"),
+        MenuData(name: "Meguro Sushi", price: "5.99", imageName: "meguro-sushi"),
+        MenuData(name: "Tako Sushi", price: "4.99", imageName: "tako-sushi"),
+        MenuData(name: "Avocado Maki", price: "2.99", imageName: "avocado-maki"),
+        MenuData(name: "Tobiko Spicy Maki", price: "4.99", imageName: "tobiko-spicy-maki"),
+        MenuData(name: "Salmon Sushi", price: "4.99", imageName: "salmon-sushi"),
+        MenuData(name: "Hamachi Sushi", price: "6.99", imageName: "hamachi-sushi"),
+        MenuData(name: "Kani Sushi", price: "3.99", imageName: "kani-sushi"),
+        MenuData(name: "Tamago Sushi", price: "3.99", imageName: "tamago-sushi"),
+        MenuData(name: "California Roll", price: "3.99", imageName: "california-roll"),
+        MenuData(name: "Shrimp Sushi", price: "3.99", imageName: "shrimp-sushi"),
+        MenuData(name: "Ikura Sushi", price: "5.99", imageName: "ikura-sushi")]
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
